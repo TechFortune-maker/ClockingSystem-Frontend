fdb02e4b37b18011fbf2aaef6145a596e58b7832
Y Ndlovu <220388239@tut4life.ac.za>
2025-03-20 10:04:04 +0100

    Added a database

---
+++ src/za/ac/tut/handler/DatabaseConnector.java
@@ -0,0 +1,22 @@
+
+package za.ac.tut.handler;
+
+import java.sql.*;
+
+public class DatabaseConnector {
+    final String URL = "jdbc:derby://localhost:1527/ClockingSystemDatabase";
+    final String USERNAME = "app";
+    final String PASSWORD = "123";
+    
+    public Connection connect() {
+        Connection connection = null;
+        
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        return connection;
+    }
+}
